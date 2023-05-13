f20845e035759da02c251c65625fc0c4b14f0fb4
maotterson <otterson.2@gmail.com>
2023-05-13 01:06:03 -0400

    implement some sample get user logic

--- BlazorWASMLocalSqliteExample/Services/UserService.cs
+++ BlazorWASMLocalSqliteExample/Services/UserService.cs
@@ -4,8 +4,10 @@ namespace BlazorWASMLocalSqliteExample.Services;
 
 public class UserService
 {
+    
     public async Task<User> GetSampleUserAsync()
     {
-
+        var readings = new List<BloodPressureReading>();
+        return new User { Guid = Guid.NewGuid(), BloodPressureReadings = readings, Name = "John Doe" };
     }
 }
