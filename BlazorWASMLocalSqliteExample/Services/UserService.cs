31c261525bac56427fcb67ecaba75aa8957f58c9
maotterson <otterson.2@gmail.com>
2023-05-13 00:59:10 -0400

    add user service and inject into nav for hyperlink by userid

---
+++ BlazorWASMLocalSqliteExample/Services/UserService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorWASMLocalSqliteExample.Models;
+
+namespace BlazorWASMLocalSqliteExample.Services;
+
+public class UserService
+{
+    public async Task<User> GetSampleUserAsync()
+    {
+
+    }
+}
