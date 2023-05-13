ee372cd01c445e2c66a45e98f001c597ef6af2f3
maotterson <otterson.2@gmail.com>
2023-05-13 00:41:25 -0400

    create sample models (blood pressure reading + user)

---
+++ BlazorWASMLocalSqliteExample/Models/BloodPressureReading.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorWASMLocalSqliteExample.Models;
+
+public class BloodPressureReading
+{
+    public Guid Guid { get; set; } = default!;
+    public Guid UserId { get; set; } = default!;
+    public User User { get; set; } = default!;
+    public DateTimeOffset DateTimeOffset { get; set; } = default!;
+    public int Systolic { get; set; } = default!;
+    public int Diastolic { get; set; } = default!;
+    public int Pulse { get; set; } = default!;
+}
---
+++ BlazorWASMLocalSqliteExample/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWASMLocalSqliteExample.Models;
+
+public class User
+{
+    public Guid Guid { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public List<BloodPressureReading> BloodPressureReadings { get; set; } = default!;
+
+}
