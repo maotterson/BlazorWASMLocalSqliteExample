c02b34ee2abb5bf03ebdddf7d835187579a59626
maotterson <otterson.2@gmail.com>
2023-05-13 23:18:52 -0400

    add dbcontext

---
+++ BlazorWASMLocalSqliteExample/Data/BPDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazorWASMLocalSqliteExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWASMLocalSqliteExample.Data;
+
+public class BPDbContext : DbContext
+{
+    public BPDbContext(DbContextOptions<BPDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<BloodPressureReading> Readings { get; set; }
+    public DbSet<User> Users { get; set; }
+}
