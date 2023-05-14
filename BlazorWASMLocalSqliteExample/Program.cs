54433ad7ffb544ef03d3ffd4c36801ebc591f772
maotterson <otterson.2@gmail.com>
2023-05-14 00:00:30 -0400

    create seeder class and random bp reading method

--- BlazorWASMLocalSqliteExample/Program.cs
+++ BlazorWASMLocalSqliteExample/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IUserService, SampleUserService>();
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddDbContext<BPDbContext>(options =>
                 options.UseSqlite("Data Source=mydatabase.db"));
 
